1c556fafdda7b6dd5c8eefaa302a768514b251af
Ali Cetinkaya <35449105+acetinkaya@users.noreply.github.com>
2026-05-06 03:41:47 +0300

    Add function to fetch TCMB currency data

--- app/ services/tcmb_service.py
+++ app/ services/tcmb_service.py
@@ -1,1 +1,10 @@
+import requests
+from app.utils.xml_parser import parse_tcmb_xml
 
+TCMB_URL = "https://www.tcmb.gov.tr/kurlar/today.xml"
+
+def get_tcmb_currency_data():
+    response = requests.get(TCMB_URL, timeout=10)
+    response.raise_for_status()
+
+    return parse_tcmb_xml(response.content)
